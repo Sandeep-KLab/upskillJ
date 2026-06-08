f58f7acba626b50863080d8fbc7c0a2a5fb6192f
sandeep-KLab <steppenwolfSSR@duck.com>
2026-06-08 16:16:32 +0530

    c03-examples

---
+++ src/test/java/com/upskillJ/c03/examples/MyFirstTest.java
@@ -0,0 +1,14 @@
+package com.upskillJ.c03.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFirstTest {
+
+    @Test
+    public void canAddTwoPlusTwo() {
+        int answer = 2+2;
+        assertEquals(4, answer,"2+2=4" );
+    }
+}
